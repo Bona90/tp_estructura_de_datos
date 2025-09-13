0c832ce1fa7c260ad2609404dc92f469c409243e
Mara16940 <marafermini@gmail.com>
2025-09-13 14:14:07 -0300

    Modificamos interfaz y comentarios

--- tp.py
+++ tp.py
@@ -1,32 +1,32 @@
 from abc import ABC, abstractmethod
 
 #  Interfaz para enviar mensajes.
-class INenviar_mensaje(ABC):
+class IEnviarMensaje(ABC):
     @abstractmethod
     def enviar_mensaje(self, destinatario, asunto, cuerpo):
         pass
 
 #  Interfaz para recibir mensajes.
-class INrecibir_mensajes(ABC):
+class IRecibirMensajes(ABC):
     @abstractmethod
     def recibir_mensaje(self, mensaje):
         pass
 
 #  Interfaz para listar mensajes.
-class INlistar_mensajes(ABC):
+class IListarMensajes(ABC):
     @abstractmethod
     def listar_mensajes(self, carpeta):
         pass
 
-class Usuario(INenviar_mensaje, INrecibir_mensajes, INlistar_mensajes):
+class Usuario(IEnviarMensaje, IRecibirMensajes, IListarMensajes):
     def __init__(self, nombre, email, password):
-        self.__nombre = nombre      #usamos con doble __ para que privado
+        self.__nombre = nombre      #  usamos encapsulamiento para proteger los datos.
         self.__email = email
         self.__password = password
         self.__carpetas = []
 
 # Getters y Setters
-    def get_nombre(self):       #usamos get xq privado
+    def get_nombre(self):       #  usamos getters y setters en los atributos que consideramos necesarios.
         return self.__nombre
     
     def set_nombre(self, nuevo_nombre):
@@ -37,6 +37,12 @@ class Usuario(INenviar_mensaje, INrecibir_mensajes, INlistar_mensajes):
     
     def set_email(self, nuevo_email):
         self.__email = nuevo_email
+
+    def set_password(self, nuevo_password):
+        self.__password = nuevo_password
+    
+    def get_carpetas(self):
+        return self.__carpetas
     
 #Metodos de Usuario
     def enviar_mensaje(self, destinatario, asunto, cuerpo):
@@ -103,7 +109,7 @@ class Carpeta:
         self.__mensajes = mensajes
 
 # Clase ServidorCorreo
-class ServidorCorreo(INenviar_mensaje, INrecibir_mensajes):
+class ServidorCorreo(IEnviarMensaje, IRecibirMensajes):
     def __init__(self, nombre):
         self.__usuarios = []  # lista de objetos Usuario
         self.__nombre = nombre
